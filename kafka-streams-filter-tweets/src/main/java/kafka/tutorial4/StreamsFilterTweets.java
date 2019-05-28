785e1a6b0abc0c4c9426f7c6a159b85d333bbaea
Rahul Gupta <rahul9ue@gmail.com>
2019-05-28 19:32:56 +0530

    Added KafkaStreams sample

---
+++ kafka-streams-filter-tweets/src/main/java/kafka/tutorial4/StreamsFilterTweets.java
@@ -0,0 +1,50 @@
+package kafka.tutorial4;
+
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class StreamsFilterTweets {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "demo-kafka-streams");
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> fromTopic = streamsBuilder.stream("twitter_tweets");
+        KStream<String, String> filterTopic = fromTopic.filter(
+                (k, jsonTweet) ->
+                    getUserFollowersFromTweet(jsonTweet) > 100
+        );
+        filterTopic.to("important_tweets");
+
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+
+        kafkaStreams.start();
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+
+    private static Integer getUserFollowersFromTweet(String value) {
+        try {
+            return jsonParser.parse(value)
+                    .getAsJsonObject()
+                    .get("user")
+                    .getAsJsonObject()
+                    .get("followers_count")
+                    .getAsInt();
+        } catch (NullPointerException e) {
+            return 0;
+        }
+    }
+}
