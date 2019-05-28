70f805770541ef61773b371ed7879ea554ae8aaa
Rahul Gupta <rahul9ue@gmail.com>
2019-05-28 15:29:10 +0530

    Added Kafka Consumer

--- kafka-consumer-elasticsearch/src/main/java/kafka/tutorial3/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/kafka/tutorial3/ElasticSearchConsumer.java
@@ -1,11 +1,17 @@
 package kafka.tutorial3;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -17,17 +23,61 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
 
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
-        String jsonMesg = "{ \"Foo\": \"Bar\" }";
         RestHighLevelClient client = createElasticSearchClient();
 
-        IndexRequest indexRequest = new IndexRequest("twitter","tweets").source(jsonMesg, XContentType.JSON);
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        logger.info("Response : " + indexResponse.getId());
+        KafkaConsumer<String, String> kafkaConsumer = getKafkaConsumer("tweets_topic");
+
+
+        while(true) {
+            ConsumerRecords<String,String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+
+            for(ConsumerRecord<String,String> record : records) {
+                String id = getTweetId(record.value());
+                IndexRequest indexRequest = new IndexRequest("twitter","tweets", id).source(record.value(), XContentType.JSON);
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                logger.info("Response : " + indexResponse.getId());
+
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+
+    private static String getTweetId(String value) {
+        return jsonParser.parse(value).getAsJsonObject().get("id_str").getAsString();
+
+    }
+
+    public static KafkaConsumer<String, String> getKafkaConsumer(String topic)
+    {
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "my-consumerDemoGroups-application";
+
+        Properties consumerProperties = new Properties();
+        consumerProperties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        consumerProperties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        consumerProperties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(consumerProperties);
+        consumer.subscribe(Collections.singleton(topic));
+        return consumer;
 
     }
 
